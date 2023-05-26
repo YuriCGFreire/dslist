52897120929c55329385c72519166a3f5217f243
YuriCGFreire <yurifreireg@gmail.com>
2023-05-26 12:18:26 -0300

    Move games on list

---
+++ src/main/java/com/devsuperior/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,22 @@
+package com.devsuperior.dslist.dto;
+
+public class ReplacementDTO {
+    Integer sourceIndex;
+    Integer destinationIndex;
+
+    public Integer getSourceIndex() {
+        return sourceIndex;
+    }
+
+    public void setSourceIndex(Integer sourceIndex) {
+        this.sourceIndex = sourceIndex;
+    }
+
+    public Integer getDestinationIndex() {
+        return destinationIndex;
+    }
+
+    public void setDestinationIndex(Integer destinationIndex) {
+        this.destinationIndex = destinationIndex;
+    }
+}
